e895902cba85746d57544e10d61cdce5c774fa4a
Alexander <semashko.fortech@gmail.com>
2022-09-28 12:07:48 +0300

    task30

---
+++ script30.js
@@ -0,0 +1,24 @@
+/*
+Имеется объект пользователя следующего вида
+{
+ id: 123,
+ first_name: 'Ivan',
+ last_name: 'Ivanov',
+ password: 'Mypwd!23',
+ age: 13
+}
+Пользуясь возможностями ES6 синтаксиса получить на основании пользователя новый объект, но без поля password.
+*/
+
+const obj = {
+  id: 123,
+  first_name: 'Ivan',
+  last_name: 'Ivanov',
+  password: 'Mypwd!23',
+  age: 13
+};
+
+const {id, first_name, last_name, age} = obj;
+const newObj = {id, first_name, last_name, age};
+
+console.log(newObj);
